40b05d9a091d4d3e6537b4998026a7bf4abff6e1
Дмитрий <@ASUS>
2017-07-17 08:19:00 +0300

    Старт работы

---
+++ 381D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _381D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
